ac07027dc4e2291c1cc0f02396fb044617101b08
kyranrana <kyran_rana@hotmail.com>
2020-03-22 17:36:31 +0000

    Updated Maps.Fundamentals example

--- src/main/groovy/Maps/Fundamentals.groovy
+++ src/main/groovy/Maps/Fundamentals.groovy
@@ -5,17 +5,23 @@ class Fundamentals {
     static void main(String[] args) {
         // In Groovy maps can be declared literally as opposed to the robust "new HashMap<>()" syntax
         // Maps can be formatted like arrays where key and value are separated by a colon:
-        def aMap = [
+        def aTypeInferredMap = [
                 "name"      : "Kyran",
                 "profession": "Software Engineer"
         ]
 
+        // Declaring the type of each key and value
+        Map<String, Integer> aMap = [
+                "test" : 123,
+                "test2": 456
+        ]
+
         // Adding new key to the map
-        aMap["sport"] = "Table Tennis"
-        assert aMap["sport"] == "Table Tennis"
+        aTypeInferredMap["sport"] = "Table Tennis"
+        assert aTypeInferredMap["sport"] == "Table Tennis"
 
         // Asserting size of map
-        assert aMap.size() == 3
+        assert aTypeInferredMap.size() == 3
     }
 
 }
