124440e683f387b2d78202fcc37b4fe08c76332d
kyranrana <kyran_rana@hotmail.com>
2020-03-22 17:26:54 +0000

    Grammar fix to TypeCheckedAnnotation description

--- src/main/groovy/Annotations/_4_TypeChecked/TypeCheckedAnnotation.groovy
+++ src/main/groovy/Annotations/_4_TypeChecked/TypeCheckedAnnotation.groovy
@@ -4,7 +4,7 @@ import groovy.transform.TypeChecked
 
 /**
  * What is TypeChecked?
- * Groovy by default is type checked at runtime, however you can change its behaviour so it checks
+ * Groovy by default is type checked at runtime however you can change its behaviour so it checks
  * types at compile time using the {@link TypeChecked} annotation. Be aware the annotated method has
  * to be free of dynamic features for {@link TypeChecked} to work.
  */
